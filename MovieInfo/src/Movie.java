018f2b4a9ec4e93a3d58235d90e252e9d1084003
Alby861 <105802322+Alby861@users.noreply.github.com>
2023-08-16 16:56:31 +0200

    Add files via upload

---
+++ MovieInfo/src/Movie.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Movie {
+    public static void main(String[] args) {
+        String movieTitle = "Indiana Jones and the Last Crusade";
+        boolean movieSeen = true;
+        int releaseDate = 1989;
+        float movieRating = 8.2f;
+        System.out.println("Movie Title: " + movieTitle);
+        System.out.println("Movie seen: " + movieSeen);
+        System.out.println("Movie released: " + releaseDate);
+        System.out.println("Movie rating: " + movieRating);
+    }
+}
